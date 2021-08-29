e2e3eefb51279a396fc6d8017be49bd24557122d
Axel <A00835275@tec.mx>
2021-08-28 20:44:32 -0500

    Ejercicio2-Tarea2

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -4,6 +4,53 @@ def main():
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
     
+    if (x > y):
+       if (z < y):
+           print(z)
+           print(y)
+           print(x)
+        
+       elif (x > z):
+           print(y)
+           print(z)
+           print(x)
+        
+       else:
+           print(y)
+           print(x)
+           print(z)
+        
+    elif (y > x):
+        if (z < x):
+            print(z)
+            print(x)
+            print(y)
+    
+        elif (z > x):
+            print(x)
+            print(z)
+            print(y)
+    
+        else:
+            print(x)
+            print(y)
+            print(z)
+
+    else:
+        if(x < y):
+            print(x)
+            print(y)
+            print(z)
+    
+        elif (y > z):
+            print(x)
+            print(z)
+            print(y)
+        
+        else:
+            print(x)
+            print(y)
+            print(z)
 
 
 if __name__=='__main__':
